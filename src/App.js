0ffd60b9bd5c063f08bb5e09adfe142b04f24004
Shammi Anand <54147419+ShammiAnand@users.noreply.github.com>
2023-04-05 15:13:14 +0530

    all containers created

--- src/App.js
+++ src/App.js
@@ -11,13 +11,19 @@ function App() {
       </div>
       <div className="App__c2">
         <div className="App__c2__r1"></div>
-        <div className="App__c2__r2"></div>
+        <div className="App__c2__r2">
+          <div className="App__c2__r2__c1"></div>
+          <div className="App__c2__r2__c2"></div>
+        </div>
         <div className="App__c2__r3"></div>
       </div>
       <div className="App__c3">
         <div className="App__c3__r1"></div>
         <div className="App__c3__r2"></div>
-        <div className="App__c3__r3"></div>
+        <div className="App__c3__r3">
+          <div className="App__c3__r3__c1"></div>
+          <div className="App__c3__r3__c2"></div>
+        </div>
       </div>
     </div>
   );
