24961570ccdf357ec1e68948a1f962ef5798d8e8
Shammi Anand <54147419+ShammiAnand@users.noreply.github.com>
2023-04-05 14:38:55 +0530

    created dashboard containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>clamp</p>
+      <div className="App__c1"></div>
+      <div className="App__c2"></div>
+      <div className="App__c3"></div>
     </div>
   );
 }
