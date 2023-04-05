bb4ee27fb880e729204becca495785df2214d0e3
Shammi Anand <54147419+ShammiAnand@users.noreply.github.com>
2023-04-05 14:48:59 +0530

    html changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="App__c1"></div>
-      <div className="App__c2"></div>
-      <div className="App__c3"></div>
+      <div className="App__c1">
+        <div className="App__c1__r1"></div>
+        <div className="App__c1__r2"></div>
+        <div className="App__c1__r3"></div>
+      </div>
+      <div className="App__c2">
+        <div className="App__c2__r1"></div>
+        <div className="App__c2__r2"></div>
+        <div className="App__c2__r3"></div>
+      </div>
+      <div className="App__c3">
+        <div className="App__c3__r1"></div>
+        <div className="App__c3__r2"></div>
+        <div className="App__c3__r3"></div>
+      </div>
     </div>
   );
 }
