16e6875fa0a632e1056418697acac32dfd66e000
Shammi Anand <54147419+ShammiAnand@users.noreply.github.com>
2023-04-05 16:37:16 +0530

    CSS updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FolderOpenOutlinedIcon from "@mui/icons-material/FolderOpenOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 
 function App() {
   return (
@@ -44,18 +45,64 @@ function App() {
             <InsertDriveFileOutlinedIcon /> Forms
           </div>
         </div>
-        <div className="App__c1__r3">Go Pro</div>
+        <div className="App__c1__r3">
+          <div
+            style={{
+              width: "90%",
+            }}
+          >
+            <p className="App__c1__r3__title">Go Pro</p>
+            <p className="App__c1__r3__desc">
+              Upgrade your plan to get pro benifits.
+            </p>
+          </div>
+          <button>
+            <strong>Let's Start</strong>
+          </button>
+        </div>
       </div>
       <div className="App__c2">
-        <div className="App__c2__r1">Total Revenue</div>
+        <div className="App__c2__r1">
+          <MonetizationOnIcon
+            style={{
+              fontSize: "4rem",
+            }}
+          />
+          <div>
+            <p className="App__c2__r1__title">Total Revenue</p>
+            <p className="App__c2__r1__cost">$92,983</p>
+          </div>
+        </div>
         <div className="App__c2__r2">
           <div className="App__c2__r2__c1">Travel</div>
           <div className="App__c2__r2__c2">Hotel</div>
         </div>
-        <div className="App__c2__r3">Free Slots Available</div>
+        <div className="App__c2__r3">
+          <p className="App__c2__r3__title">Free Slots Available</p>
+          <div className="App__c2__r3__dates">
+            <div
+              className="App__c2__r3__date"
+              style={{
+                backgroundColor: "#ee515a",
+              }}
+            ></div>
+            <div className="App__c2__r3__date"></div>
+            <div className="App__c2__r3__date"></div>
+            <div className="App__c2__r3__date"></div>
+            <div className="App__c2__r3__date"></div>
+            <div className="App__c2__r3__date"></div>
+          </div>
+        </div>
       </div>
       <div className="App__c3">
-        <div className="App__c3__r1">Dear Client</div>
+        <div className="App__c3__r1">
+          <div style={{ width: "70%" }}>
+            <p className="App__c3__r1__title">Dear Client</p>
+            <p className="App__c3__r1__desc">
+              Join our online community. It helps in managing your projects.{" "}
+            </p>
+          </div>
+        </div>
         <div className="App__c3__r2">Transaction details</div>
         <div className="App__c3__r3">
           <div className="App__c3__r3__c1">Proj Detail</div>
